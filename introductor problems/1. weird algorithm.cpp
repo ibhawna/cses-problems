c7c36a44e8fbdd3b45d96eae944160368f78e518
Bhawna <rathibhawna18@gmail.com>
2022-09-06 16:05:47 +0530

    add changes

---
+++ introductor problems/1. weird algorithm.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+  cin>>n;
+  while(n!=1){
+    cout<<n<<" ";
+    if(n&1){ // odd
+      n = n*3 + 1;
+    }
+    else{
+      n = n/2;
+    }
+  }
+  cout<<"1";
+}
+
+
+
+// using bitwise operator as divide by 2
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+  cin>>n;
+  
+  while(n!=1){
+    if(n&1){
+      n = n*3 + 1;
+    }
+    else{
+      n = n >> 1; // everytime we shift a number towards right by 1 bit it divides by number by 2 .
+    }
+    cout<<n<<" ";
+  }
+  cout<<"1";
+}
+
+

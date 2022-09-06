a2d963bee2a9b730715c4eb553bb27da6d0fb71f
Bhawna <rathibhawna18@gmail.com>
2022-09-06 16:08:41 +0530

    changed code

--- introductor problems/1. weird algorithm.cpp
+++ introductor problems/1. weird algorithm.cpp
@@ -1,20 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main() {
-    long long n;
-  cin>>n;
-  while(n!=1){
-    cout<<n<<" ";
-    if(n&1){ // odd
-      n = n*3 + 1;
-    }
-    else{
-      n = n/2;
-    }
-  }
-  cout<<"1";
-}
+// int main() {
+//     long long n;
+//   cin>>n;
+//   while(n!=1){
+//     cout<<n<<" ";
+//     if(n&1){ // odd
+//       n = n*3 + 1;
+//     }
+//     else{
+//       n = n/2;
+//     }
+//   }
+//   cout<<"1";
+// }
 
 
 
@@ -27,13 +27,13 @@ int main() {
   cin>>n;
   
   while(n!=1){
+    cout<<n<<" ";
     if(n&1){
       n = n*3 + 1;
     }
     else{
       n = n >> 1; // everytime we shift a number towards right by 1 bit it divides by number by 2 .
     }
-    cout<<n<<" ";
   }
   cout<<"1";
 }
